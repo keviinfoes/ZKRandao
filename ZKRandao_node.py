ff2f03c1bfdf9e9f7039d445383e57c14127ae2a
Kevin Foesenek <kevin.foes@live.nl>
2019-08-14 16:06:49 +0200

    Added submit transaction
    Added submit transaction

--- ZKRandao_node.py
+++ ZKRandao_node.py
@@ -1,9 +1,10 @@
-#Implementation ZKRandao node
+#Concept implementation ZKRandao node
 
 from web3 import Web3
 import random
 import subprocess
 import os
+import json
 
 #Set node for Ropsten
 web3_ZKRandao = Web3(Web3.HTTPProvider("https://ropsten.infura.io/v3/a6eaf73151ac4cd386fab484134d4038"))
@@ -13,8 +14,10 @@ abi_ZKRandao = '''[{"constant":true,"inputs":[],"name":"RevealRangeOther","outpu
 address_ZKRandao = web3_ZKRandao.toChecksumAddress("0x3D6A7061Bb4F3dDd33cBfeaE24dc72167aE51281")
 contract_ZKRandao = web3_ZKRandao.eth.contract(address_ZKRandao, abi=abi_ZKRandao)
 
-#Set blocknumber counter
+#Set blocknumber, submit and reveal counter
 indexBlockNumber = web3_ZKRandao.eth.blockNumber
+indexSubmitRN = 0
+indexRevealRN = 0
 
 #Set boundaries based on ZKRandao contract
 ExpectedRange = contract_ZKRandao.functions.ExpRange().call()
@@ -66,20 +69,49 @@ def handle_event(block_filter):
         Secret_place = ExpectedRange - Secret
         with cd("/Users/kevinfoesenek/Desktop/TEST_ZKrandao0.1/ZKRandao_submit"):
             subprocess.run(["/Users/kevinfoesenek/.zokrates/bin/zokrates", "compute-witness", "-a", "0", "0", "0", str(Secret),
-                           str(out_0), str(out_1), str(ExpectedRange), str(Secret_place)])
-            subprocess.run(["/Users/kevinfoesenek/.zokrates/bin/zokrates", "generate-proof"])
+                           str(out_0), str(out_1), str(ExpectedRange), str(Secret_place)], stdout=subprocess.DEVNULL)
+            subprocess.run(["/Users/kevinfoesenek/.zokrates/bin/zokrates", "generate-proof"], stdout=subprocess.DEVNULL)
 
-        
-        
-## TO DO - ADD REVEAL CHECK SUBMIT AND THEN REVEAL AFTER X BLOCKS
-        
-        
-        #Run ZoKrates for the reveal proof
-        with cd("/Users/kevinfoesenek/Desktop/TEST_ZKrandao0.1/ZKRandao_reveal"):
-            subprocess.run(["/Users/kevinfoesenek/.zokrates/bin/zokrates", "compute-witness", "-a", "0", "0", "0", str(Secret),
-                            str(out_0), str(out_1), str(ExpectedRange), str(Secret_place)])
-            subprocess.run(["/Users/kevinfoesenek/.zokrates/bin/zokrates", "generate-proof"])
+            #Create raw transaction for infura
+            # Create signed transaction for infura
+            nonce = web3_ZKRandao.eth.getTransactionCount(web3_ZKRandao.toChecksumAddress("0x4B1366383c1f592Cfe00ab8FB031Fe4D56Ae680e"))
 
+            #Read proof data in json file
+            with open("/Users/kevinfoesenek/Desktop/TEST_ZKrandao0.1/ZKRandao_submit/proof.json") as json_file:
+                data = json.load(json_file)
+                data_proof = data['proof']
+                submit_a = data_proof['a']
+                submit_b = data_proof['b']
+                submit_c = data_proof['c']
+                inputs = data['inputs']
+
+                #Create empty array to add the data
+                a_array = []
+                b_array_total = []
+                b_firsarray = []
+                b_secondarray = []
+                c_array = []
+                inputs_array = []
+                a_array.extend([int(submit_a[0], base=16), int(submit_a[1], base=16)])
+                b_firsarray.extend([int(submit_b[0][0], base=16), int(submit_b[0][1], base=16)])
+                b_secondarray.extend([int(submit_b[1][0], base=16), int(submit_b[1][1], base=16)])
+                b_array_total.extend([b_firsarray, b_secondarray])
+                c_array.extend([int(submit_c[0], base=16), int(submit_c[1], base=16)])
+                inputs_array.extend([int(inputs[0], base=16), int(inputs[1], base=16), int(inputs[2], base=16),
+                                     int(inputs[3], base=16)])
+
+            contract_txn = contract_ZKRandao.functions.submitRN(a_array,
+                                                                b_array_total,
+                                                                c_array,
+                                                                inputs_array).buildTransaction({'gas': 857144, 'nonce': nonce})
+
+            private_key = "A3076896C1E73B1079955F2FE72CFF4015DC7900DBD7321D0119004310EE716E"
+            signed_txnDeposit = web3_ZKRandao.eth.account.signTransaction(contract_txn, private_key)
+            web3_ZKRandao.eth.sendRawTransaction(signed_txnDeposit.rawTransaction)
+            print("Transaction succes")
+            
+#WRITE REVEAL FUNCTION            
+            #ADD BASED ON SUBMIT FUNCTION
 
 
 def main():
@@ -88,6 +120,7 @@ def main():
         TEMPindexBlockNumber = web3_ZKRandao.eth.blockNumber
         if TEMPindexBlockNumber > indexBlockNumber:
             indexBlockNumber += 1
+            #if indexBlockNumber % 20 == 0: #Start submit proces every 20 blocks
             block_filter = web3_ZKRandao.eth.getBlock(indexBlockNumber)
             handle_event(block_filter)
     else:
